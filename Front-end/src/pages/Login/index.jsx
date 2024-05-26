e6150803974fd8d32e3109ca7db6f5740d52aaee
Patrick E.S Silva <patricke.ssilva@gmail.com>
2024-05-26 03:14:40 -0300

    Criação da página Login (Base)

--- Front-end/src/pages/Login/index.jsx
+++ Front-end/src/pages/Login/index.jsx
@@ -1,12 +1,32 @@
-import {Title} from './styles'
+import {Title, MainLogin, Div, DivTitle, Divlogin, Img, DivMain, Input, Subtitle, Button} from './styles'
 
 function Login(){
 
     return(
-        <div>
-            
-            <Title>FAÇA SEU LOGIN</Title>
-        </div>
+        <Div>
+            <MainLogin>
+                <DivMain>
+
+                    <DivTitle>
+                         <Title>FAÇA SEU LOGIN</Title>
+                    </DivTitle>
+                    
+                        <Divlogin>
+                            <Subtitle>Usuário</Subtitle>
+                            <Input></Input>
+                         </Divlogin>
+
+                         <Divlogin>
+                            <Subtitle>Senha</Subtitle>
+                            <Input></Input>
+                         </Divlogin>
+                            <Button>Entrar</Button>
+                 </DivMain>
+                    <Img>
+                    </Img>
+                    
+            </MainLogin>
+        </Div>
     )
 }
 export default Login;  
