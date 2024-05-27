a33afbd81928e8fec5a64f48a12fe34bdea327da
Patrick E.S Silva <patricke.ssilva@gmail.com>
2024-05-27 00:18:34 -0300

    Criação da tag Global Button

---
+++ Front-end/src/components/Button/index.jsx
@@ -0,0 +1,11 @@
+import { ButtonGlobal } from "./styles";
+
+function Button(){
+
+    return(
+        <>
+        <ButtonGlobal></ButtonGlobal>
+        </>
+    )
+}
+export default Button;  
---
+++ Front-end/src/components/Button/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+export const ButtonGlobal = styled.button`
+    background:linear-gradient(to right, #F9DB5C,#ffc200);
+    color: black;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    width: 45vh;
+    height: 40px;
+    border-radius: 15px;
+    cursor: pointer;
+    border: none;
+    font-size: 30px;
+    transition: font-size 0.3s;
+    
+    &:hover{
+    background:linear-gradient(to right, #ffc200,#F9DB5C);
+    font-size: 35px;
+
+    }
+`
