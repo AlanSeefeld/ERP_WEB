25992a839eb67a2d3f03537b5e2ddbda3f838efe
Patrick E.S Silva <patricke.ssilva@gmail.com>
2024-05-26 23:57:13 -0300

    Criação da tela Home (Base)

---
+++ Front-end/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import { Div } from "./styles";
+import Header from "../../components/Header";
+
+function Home(){
+
+    return(
+        <Div>
+            <Header></Header>
+            <img src="" alt="Logo-Royf" />
+        </Div>
+    )
+}
+export default Home;  
---
+++ Front-end/src/pages/Home/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const Div = styled.div`
+    justify-content: center;
+    display: flex;
+    flex: 1;
+    align-items: center;
+`
