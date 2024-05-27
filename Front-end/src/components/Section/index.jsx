7ee8e2da27e7457603ae3136121d7b254a1ad66c
Patrick E.S Silva <patricke.ssilva@gmail.com>
2024-05-27 00:29:37 -0300

    Criação da tag global Section

---
+++ Front-end/src/components/Section/index.jsx
@@ -0,0 +1,11 @@
+import { SectionGlobal } from "./styles"; 
+
+function Section(){
+
+    return(
+        <>
+            <SectionGlobal></SectionGlobal>
+        </>
+    )
+}
+export default Section;  
