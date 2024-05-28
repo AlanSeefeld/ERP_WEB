b326e75625fcb77004c3587b1a47f06717ec9350
Patrick E.S Silva <patricke.ssilva@gmail.com>
2024-05-28 00:12:44 -0300

    Criado tag global TitleInput

---
+++ Front-end/src/components/TitleInput/index.jsx
@@ -0,0 +1,10 @@
+import { DivTitle } from "./styles"
+
+function TitleInput ({children}){
+    return(
+        <>
+            <DivTitle>{children}</DivTitle>
+        </>
+    )
+}
+export default TitleInput;
---
+++ Front-end/src/components/TitleInput/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const DivTitle = styled.div`
+    color: black;
+    width: 100%;
+    height: 25px;
+    padding-left: 30px;
+
+    font-size: 25px;
+    font-weight: bold;
+    font-family: Arial, Helvetica, sans-serif;
+
+`
