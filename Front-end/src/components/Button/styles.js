0cb602563b5628a1c5e03f2d380396fa37d39e6d
Patrick E.S Silva <patricke.ssilva@gmail.com>
2024-05-30 21:28:27 -0300

    Ajuste de sombra no button

--- Front-end/src/components/Button/styles.js
+++ Front-end/src/components/Button/styles.js
@@ -1,22 +1,24 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const ButtonGlobal = styled.button`
-    background:linear-gradient(to right, #F9DB5C,#ffc200);
-    color: black;
-    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    width: 45vh;
-    height: 40px;
-    border-radius: 15px;
-    cursor: pointer;
-    border: none;
-    font-size: 30px;
-    transition: font-size 0.3s;
-    z-index: 9999;
-    position: absolute;
-    
-    &:hover{
-    background:linear-gradient(to right, #ffc200,#F9DB5C);
-    font-size: 35px;
+  background: linear-gradient(to right, #f9db5c, #ffc200);
+  color: black;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  width: 45vh;
+  height: 40px;
+  border-radius: 15px;
+  cursor: pointer;
+  border: none;
+  font-size: 30px;
+  transition: font-size 0.3s;
+  z-index: 9999;
+  position: absolute;
+  -webkit-box-shadow: 10px 10px 21px -8px rgba(0, 0, 0, 0.61);
+  -moz-box-shadow: 10px 10px 21px -8px rgba(0, 0, 0, 0.61);
+  box-shadow: 10px 10px 21px -8px rgba(0, 0, 0, 0.61);
 
-    }
-`
+  &:hover {
+    background: linear-gradient(to right, #ffc200, #f9db5c);
+    font-size: 35px;
+  }
+`;
